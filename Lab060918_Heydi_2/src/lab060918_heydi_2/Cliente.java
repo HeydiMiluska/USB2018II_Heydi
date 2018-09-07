715d92b0578100f4fe80a595130d1d81d6064005
HeydiMiluska <miluskita0797@gmail.com>
2018-09-06 20:51:13 -0500

    Prueba 1
    ...

---
+++ Lab060918_Heydi_2/src/lab060918_heydi_2/Cliente.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab060918_heydi_2;
+
+/**
+ *
+ * @author Programacion
+ */
+public class Cliente {
+     String Nombre, Apellido, Dni, Edad;
+    double Peso, Estatura, Color;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getDni() {
+        return Dni;
+    }
+
+    public void setDni(String Dni) {
+        this.Dni = Dni;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    public double getPeso() {
+        return Peso;
+    }
+
+    public void setPeso(double Peso) {
+        this.Peso = Peso;
+    }
+
+    public double getEstatura() {
+        return Estatura;
+    }
+
+    public void setEstatura(double Estatura) {
+        this.Estatura = Estatura;
+    }
+
+    public double getColor() {
+        return Color;
+    }
+
+    public void setColor(double Color) {
+        this.Color = Color;
+    }
+    
+    
+}
---
+++ Lab060918_Heydi_2/src/lab060918_heydi_2/Lab060918_Heydi_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab060918_heydi_2;
+
+/**
+ *
+ * @author Programacion
+ */
+public class Lab060918_Heydi_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
